66f41899ac8c705d3187dbbef3cd8446726c264e
Patrick Almeida <patricksalmeida@gmail.com>
2021-03-08 21:11:34 -0300

    Add animations into notes list

--- src/app/pages/notes-list/notes-list.component.ts
+++ src/app/pages/notes-list/notes-list.component.ts
@@ -1,3 +1,4 @@
+import { animate, query, stagger, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 import { NotesService } from 'src/app/core/services/notes.service';
 import { Note } from 'src/app/models/note.model';
@@ -5,7 +6,67 @@ import { Note } from 'src/app/models/note.model';
 @Component({
   selector: 'app-notes-list',
   templateUrl: './notes-list.component.html',
-  styleUrls: ['./notes-list.component.scss']
+  styleUrls: ['./notes-list.component.scss'],
+  animations: [
+    trigger('itemAnim', [
+      transition('void => *', [
+        style({
+          height: 0,
+          opacity: 0,
+          transform: 'scale(0.05)',
+          'margin-bottom': 0,
+          paddingTop: 0,
+          paddingBottom: 0,
+          paddingRight: 0,
+          paddingLeft: 0,
+        }),
+        animate('50ms', style({
+          height: '*',
+          'margin-bottom': '*',
+          paddingTop: '*',
+          paddingBottom: '*',
+          paddingRight: '*',
+          paddingLeft: '*',
+        })),
+        animate(68)
+      ]),
+      transition('* => void', [
+        animate(50, style({
+          transform: 'scale(1.05)',
+        })),
+        animate(50, style({
+          transform: 'scale(1)',
+          opacity: 0.75
+        })),
+        animate('120ms ease-out', style({
+          transform: 'scale(0.68)',
+          opacity: 0
+        })),
+        animate('150ms ease-out', style({
+          height: 0,
+          paddingTop: 0,
+          paddingBottom: 0,
+          paddingRight: 0,
+          paddingLeft: 0,
+          'margin-bottom': 0,
+        }))
+      ])
+    ]),
+
+    trigger('listAnim', [
+      transition('* => *', [
+        query(':enter', [
+          style({
+            opacity: 0,
+            height: 0
+          }),
+          stagger(100, [
+            animate('0.2s ease')
+          ])
+        ], { optional: true })
+      ])
+    ])
+  ]
 })
 export class NotesListComponent implements OnInit {
 
