7463a64f16a6f046275cd49d0f71d5bb5df22d5d
Patrick Almeida <patricksalmeida@gmail.com>
2021-03-09 18:48:35 -0300

    Add filter method

--- src/app/pages/notes-list/notes-list.component.ts
+++ src/app/pages/notes-list/notes-list.component.ts
@@ -71,14 +71,48 @@ import { Note } from 'src/app/models/note.model';
 export class NotesListComponent implements OnInit {
 
   notes: Note[] = new Array<Note>();
+  filteredNotes: Note[] = new Array<Note>();
 
   constructor(private notesService: NotesService) { }
 
   ngOnInit(): void {
     this.notes = this.notesService.getAll();
+    this.filteredNotes = this.notes;
   }
 
   deleteNote(id: number) {
     this.notesService.delete(id);
+  }
+
+  filter(query: string) {
+    query = query.toLowerCase().trim();
+
+    let allResults: Note[] = new Array<Note>();
+
+    let terms: string[] = query.split(' ');
+    terms = this.removeDuplicates(terms);
+
+    terms.forEach(term => {
+      let results = this.relevantNotes(term)
+      allResults = [...allResults, ...results]
+    });
+
+    let uniqueResults = this.removeDuplicates(allResults);
+    
+    this.filteredNotes = uniqueResults;
+  }
+
+  removeDuplicates(arr: Array<any>): Array<any> {
+    let uniqueResults: Set<any> = new Set<any>();
+
+    arr.forEach(e => uniqueResults.add(e))
+
+    return Array.from(uniqueResults)
+  }
+
+  relevantNotes(query: string): Array<Note> {
+    query = query.toLowerCase().trim();
+
+    return this.notes.filter(note => note.title && note.title.toLowerCase().includes(query))
   }
 }
